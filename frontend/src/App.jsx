904e0dc4e91a368b584143aef5cc4aa9789473d3
Abhiram Agina <abhiram_agina@umail.ucsb.edu>
2025-12-26 16:16:34 -0800

    updating frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from "./components/Navbar"
 import './App.css'
 
+function AuthButtons() {
+  const startGoogle = () => {
+    window.location.href = "/auth/google/start";
+  };
+
+  return (
+    <div style = {{display: "flex", gap: 12}}>
+      <button onClick={startGoogle}>Login</button>
+    </div>
+  );
+}
+
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me", {credentials: "include"})
+    .then((r) => r.json())
+    .then((data) => setUser(data.user));
+  }, []);
+
   return (
     <div style = {{fontFamily: "system-ui", padding: 24}}>
       <h1>IV Events</h1>
       <p>See what's going on in and around UC Santa Barbara!</p>
+      <Navbar />
+      <AuthButtons />
+      {user ? <div>Logged in as {user.email}.</div> : <div>Not logged in.</div>}
     </div>
   )
 }
