1ef1c090543eac24ab904fd03ab3ec90d4454058
Abhiram Agina <abhiram_agina@umail.ucsb.edu>
2025-12-26 16:47:32 -0800

    adding navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+// src/components/Navbar.jsx
+export default function Navbar() {
+  return (
+    <div style={{ display: "flex", gap: 12, marginTop: 16, marginBottom: 16 }}>
+      <a href="/" style={{ textDecoration: "none" }}>Home</a>
+    </div>
+  );
+}
